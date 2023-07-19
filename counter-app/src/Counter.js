01dd8071f6b44833d09c5c40048d091e57d59948
Tehrim Ahmed <tehrimahmed2002@gmail.com>
2023-07-19 14:45:23 +0500

    update

--- counter-app/src/Counter.js
+++ counter-app/src/Counter.js
@@ -1,48 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
+export default function Counter() {
+  const [count, setCount] = useState(0);
 
-  increment() {
-    this.setState((prevState) => ({
-      count: prevState.count + 1
-    }));
-    console.log(this.state.count);
-  }
+  const handleIncrement = () => (
+    setCount(count + 1)
+  );
 
-  decrement() {
-    this.setState((prevState) => ({
-      count: prevState.count - 1
-    }));
-    console.log(this.state.count);
-  }
+  const handleDecrement = () => (
+    setCount(count - 1)
+  );
 
-  render() {
-    return (
-      <div className="container mt-5">
-        <h1 className="row justify-content-center" style={{ fontWeight: 'bold'}}>Counter App</h1>
-        <div className="row justify-content-center">
-          <div className="col-2 text-center">
-            <h2 style={{ fontWeight: 'bold' }}>Count: {this.state.count}</h2>
-          </div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-2 text-center">
-            <button className="btn btn-primary" onClick={() => this.decrement()}>-</button>
-          </div>
-          <div className="col-2 text-center">
-            <button className="btn btn-primary" onClick={() => this.increment()}>+</button>
-          </div>
+  return (
+    <div className="container mt-5">
+      <h1 className="row justify-content-center" style={{ fontWeight: 'bold' }}>Counter App</h1>
+      <div className="row justify-content-center">
+        <div className="col-2 text-center">
+          <h2 style={{ fontWeight: 'bold' }}>Count: {count}</h2>
         </div>
       </div>
-    );
-  }
+      <div className="row justify-content-center">
+        <div className="col-2 text-center">
+          <button className="btn btn-primary" onClick={handleDecrement}>-</button>
+        </div>
+        <div className="col-2 text-center">
+          <button className="btn btn-primary" onClick={handleIncrement}>+</button>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default Counter;
